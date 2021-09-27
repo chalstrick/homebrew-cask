38dbfc79e2da05cdf5ec775f47bb6e3dc695e9c2
Christian Halstrick <christian.halstrick@sap.com>
2021-09-27 11:23:57 +0200

    Update sapmachine-jdk to contain two platforms for macos

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -1,9 +1,14 @@
 cask "sapmachine-jdk" do
   version "17"
-  sha256 "0b29168998bf0a72107aca7eff5a1d80a9dacbe5af5cbdbf6026a746ff093dfe"
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg",
-      verified: "github.com/SAP/SapMachine/"
+  if Hardware::CPU.intel?
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-x64_bin.dmg"
+    sha256 "0b29168998bf0a72107aca7eff5a1d80a9dacbe5af5cbdbf6026a746ff093dfe"
+  else
+    url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_macos-aarch64_bin.dmg"
+    sha256 "e993513eb8837399507009ebbc8f980de6fd57ce55196a2e102bfd7fb617530f"
+  end
+
   name "SapMachine OpenJDK Development Kit"
   desc "OpenJDK release maintained and supported by SAP"
   homepage "https://sapmachine.io/"
